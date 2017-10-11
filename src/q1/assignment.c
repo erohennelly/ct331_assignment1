c92ae587eacbdb812ac2d3e59b395a18deb2531f
erohennelly <erohennelly@hotmail.com>
2017-10-11 17:52:53 +0100

    Add files via upload

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -2,15 +2,13 @@
 
 int main(int arg, char* argc[]){
   // create variables of the following types: int, int*, long, double*, char**
-  int a;
-  int* b;
-  long c;
-  double* d;
-  char** e;
-  printf("the size of int is: %zu\n",sizeof(a));
-  printf("the size of int pointer is: %zu\n",sizeof(b));
-  printf("the size of long is: %zu\n",sizeof(c));
-  printf("the size of double pointer is: %zu\n",sizeof(d));
-  printf("the size of a char pointer is: %zu\n",sizeof(e));
+
+
+  printf("Sizeof int: %i \n", sizeof(int));
+  printf("Sizeof int*: %i \n", sizeof(int*));
+  printf("Sizeof long: %ld \n", sizeof(long));
+  printf("Sizeof double*: %u \n", sizeof(double*));
+  printf("Sizeof char**: %u \n", sizeof(char**));
+  
   return 0;
 }
