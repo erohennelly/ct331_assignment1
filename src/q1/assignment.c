2fb1b2ff226c849e51b594cca9c3082fdc45e657
erohennelly <erohennelly@hotmail.com>
2017-10-11 17:26:37 +0100

    attempt

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+  // create variables of the following types: int, int*, long, double*, char**
+  int a;
+  int* b;
+  long c;
+  double* d;
+  char** e;
+  printf("the size of int is: %zu\n",sizeof(a));
+  printf("the size of int pointer is: %zu\n",sizeof(b));
+  printf("the size of long is: %zu\n",sizeof(c));
+  printf("the size of double pointer is: %zu\n",sizeof(d));
+  printf("the size of a char pointer is: %zu\n",sizeof(e));
   return 0;
 }
